bdc540a56578b21ae49dc5305b10d1952f4d0d90
Nenad Radulovic <nenad.radulovic@digitalinsurance.io>
2024-02-21 15:48:45 +0100

    fix tests

--- src/tests/__mocks__/prisma.ts
+++ src/tests/__mocks__/prisma.ts
@@ -1,8 +1,8 @@
 // libs/__mocks__/prisma.ts
 // 1
-import { PrismaClient } from "@prisma/client";
-import { beforeEach } from "vitest";
-import { mockDeep, mockReset } from "vitest-mock-extended";
+import { PrismaClient } from '@prisma/client';
+import { beforeEach } from 'vitest';
+import { mockDeep, mockReset } from 'vitest-mock-extended';
 
 // 2
 beforeEach(() => {
